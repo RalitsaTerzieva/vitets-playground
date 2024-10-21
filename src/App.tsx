d1b1ede891912bedbdd91d14b534731153f2ed78
Ralica Popova <ralicaterzieva90@gmail.com>
2024-10-21 12:40:08 +0300

    Add Greeting component and refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-type AppProps = {
-  message: string;
-};
+import Greeting from "./Greetings";
+import UserCard from "./UserCard";
+import Button from "./Button";
+import Counter from "./Counter";
+import InputField from "./InputField";
+import InputWithRef from "./InputWithRef";
 
-function App({ message }: AppProps) {
-  return <div>{message}</div>;
+function App() {
+  return (
+    <div>
+      <Greeting name="Mike" />
+      <UserCard user={{ name: "Mike", email: "mikes@email.com" }} />
+      <Button>Click me!</Button>
+      <Button disabled>Don't click me!</Button>
+      <Counter />
+      <InputField />
+      <InputWithRef />
+    </div>
+  );
 }
 
 export default App;
